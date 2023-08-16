9d78879654826960d59ff020d73eb7acb3e857dc
midhun_tc <midhun@excelledia.com>
2023-08-16 17:23:04 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboadComponent } from './dashboad/dashboad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboadComponent
   ],
   imports: [
     BrowserModule,
